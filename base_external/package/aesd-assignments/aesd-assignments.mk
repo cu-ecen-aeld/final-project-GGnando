1aa786f2ea380abc194f222e187cf00d129b816d
GGnando <nelio1996@gmail.com>
2025-04-28 05:06:27 -0600

    Added working raspberry pi kernel

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,23 +6,26 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
+AESD_ASSIGNMENTS_VERSION = dad37a5e6e101ecbd602600461da5bd3379c8773
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
-AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+# AESD_ASSIGNMENTS_SITE = git@github.com:GGnando/aesd_project.git
+# AESD_ASSIGNMENTS_SITE_METHOD = git
+AESD_ASSIGNMENTS_SITE = $(TOPDIR)/../../aesd_project
+
+AESD_ASSIGNMENTS_SITE_METHOD = local
+# AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = project
+AESD_ASSIGNMENTS_DEPENDENCIES = pigpio
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/servo_daemon all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+    $(INSTALL) -D -m755 $(@D)/servo_daemon/servo_daemon  $(TARGET_DIR)/usr/bin/servo_daemon
 endef
 
 $(eval $(generic-package))
---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+pushd buildroot
+make distclean
+popd
