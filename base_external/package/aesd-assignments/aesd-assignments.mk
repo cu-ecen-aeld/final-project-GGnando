c4a1c6693604db0adc6c153e7657596157a719e3
GGnando <nelio1996@gmail.com>
2025-04-29 03:00:48 -0600

    Sprint 3 complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,16 +6,16 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = dad37a5e6e101ecbd602600461da5bd3379c8773
+AESD_ASSIGNMENTS_VERSION = c49b63ae7d56eb5f5fd07ff15932041f3ecdce11
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-# AESD_ASSIGNMENTS_SITE = git@github.com:GGnando/aesd_project.git
-# AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_SITE = $(TOPDIR)/../../aesd_project
+AESD_ASSIGNMENTS_SITE = https://github.com/GGnando/UltrasonicRangingPlatform
+AESD_ASSIGNMENTS_SITE_METHOD = git
+# AESD_ASSIGNMENTS_SITE = $(TOPDIR)/../../UltrasonicRangingPlatform
 
-AESD_ASSIGNMENTS_SITE_METHOD = local
-# AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+# AESD_ASSIGNMENTS_SITE_METHOD = local
+AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 AESD_ASSIGNMENTS_MODULE_SUBDIRS = project
 AESD_ASSIGNMENTS_DEPENDENCIES = pigpio
 
@@ -23,7 +23,6 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/servo_daemon all
 endef
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m755 $(@D)/servo_daemon/servo_daemon  $(TARGET_DIR)/usr/bin/servo_daemon
 endef
